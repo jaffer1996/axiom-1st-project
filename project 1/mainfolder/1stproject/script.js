03443dd3fbc31c05e6d0c5930dc4888a8caf5322
jaffer1996 <jaffermansoor18.jm@gmail.com>
2021-05-03 15:34:56 +0500

    final changes in script.js
    email validation, password matching, length checks added

--- project 1/mainfolder/1stproject/script.js
+++ project 1/mainfolder/1stproject/script.js
@@ -16,19 +16,14 @@ const showSuccess = (input) => {
     formControl.classList.add('success');
 }
 
-function validateEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-password.addEventListener('change', (e) => {
-    if(e.target.value.length <= 5){
-        showError(password, 'Password is too weak');
-        console.log(e.target.value);
+    if ( re.test(input.value.trim())) {
+        showSuccess(input)
     } else {
-        showSuccess(password);
+        showError(input, 'Email not valid')
     }
-})
+}
 
 const checkrequired = (inputarray) => {
     inputarray.forEach(element => {
@@ -41,6 +36,24 @@ const checkrequired = (inputarray) => {
     });
 } 
 
+const checklength = (input, min, max) => {
+    if (input.value.length < min) {
+        showError(input, 'not enough characters')
+    } else if (input.value.length > max){
+        showError(input, 'too many characters')
+    } else {
+        showSuccess(input)
+    }
+}
+
+const checkpasswordmatch = (input1, input2) => {
+    if (input1.value !== input2.value ) {
+        showError (input2, 'passwords do not match')
+    } else {
+        showSuccess (input2)
+    }
+}
+
 const nameformat = (input) => {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1)
 }
@@ -49,5 +62,8 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     
     checkrequired([username,email,password,password2]);
-
+    checklength(username,3,10);
+    checklength(password,6,30);
+    checkEmail(email);
+    checkpasswordmatch(password,password2);
 })
