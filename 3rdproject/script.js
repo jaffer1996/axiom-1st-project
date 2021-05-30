fa4ba5d8e0f8047634817436ff1fc8e593ca5778
jaffer1996 <jaffermansoor18.jm@gmail.com>
2021-05-30 18:59:39 +0500

    changes in project 3
    added timestamp 2

--- 3rdproject/script.js
+++ 3rdproject/script.js
@@ -3,6 +3,7 @@ const play = document.getElementById('play');
 const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
+const timestamp2 = document.getElementById('timestamp2');
 
 const togglevideostatus = () => {
     if(video.paused) {
@@ -27,13 +28,23 @@ const updateprogress = () => {
     if (mins < 10){
         mins = '0' + String(mins);
     }
-
     let secs = Math.floor(video.currentTime % 60);
     if (secs < 10){
         secs = '0' + String(secs);
     }
 
     timestamp.innerHTML = `${mins}:${secs}`;
+
+    let mins2 = Math.floor(video.duration / 60);
+    if (mins2 < 10){
+        mins2 = '0' + String(mins2);
+    }
+    let secs2 = Math.floor(video.duration % 60);
+    if (secs2 < 10){
+        secs2 = '0' + String(secs2);
+    }
+
+    timestamp2.innerHTML = `${mins2}:${secs2}`;
 }
 
 const stopvideo = () => {
